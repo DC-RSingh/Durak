32b4bc5b749b8a3ecea3fc9102e04160474de6d8
Dalton798 <56048637+Dalton798@users.noreply.github.com>
2021-02-15 17:40:14 -0500

    Add files via upload

---
+++ OOP3_Durak/HomeScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP3_Durak
+{
+    public partial class HomeScreen : Form
+    {
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("This is the final project for OOP4200, group 4. We have created a working version of the card game 'Durak'." +
+                " It was created by Gabriel Dietrich, Fleur Blanckaert, Raje Singh, and Dalton Young. ");
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.pagat.com/beating/podkidnoy_durak.html");
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.ShowDialog();
+        }
+    }
+}
