b147e6ef4a8a84d01bbd025b1d3e7dc402b5b545
DC-RSingh <raje.singh@dcmail.ca>
2021-03-02 22:05:30 -0500

    Merge updated 'main' to dalton-dev

---
+++ CardLib/Face.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLib
+{
+    public enum Face
+    {
+        /// <summary>
+        /// Represents card facing up
+        /// </summary>
+        Down,
+
+        /// <summary>
+        /// Represents card facing down
+        /// </summary>
+        Up
+    }
+}
---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        { 
+            DurakWindow gameWindow = new DurakWindow();
+            gameWindow.ShowDialog();
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is the final project for OOP4200, group 4. We have created a working version of the card game 'Durak'." +
+                " It was created by Gabriel Dietrich, Fleur Blanckaert, Raje Singh, and Dalton Young. ");
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.pagat.com/beating/podkidnoy_durak.html");
+        }
+
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
