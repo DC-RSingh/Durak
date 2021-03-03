eeabff085f17d83be7e3f5c648b6c1700823bc77
Raje Singh <raje.singh@dcmail.ca>
2021-03-02 20:55:45 -0500

    moved Card dependent stuff to CardLib

--- Client/DurakWindow.xaml.cs
+++ Client/DurakWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using CardLib;
+using CardUI;
 
 namespace Client
 {
@@ -27,11 +29,21 @@ namespace Client
             InitializeComponent();
             DisplayHand();
         }
-        const string PACK_PATH = @"pack://application:,,,/Resources/Pack/";
+        //const string PACK_PATH = @"pack://application:,,,/Resources/Pack/";
+        
+        // Testing Displaying the Card
         private void DisplayHand()
         {
+            // Create Face Up Card
+            var test_card = new PlayingCard(Suit.Club, Rank.King, Face.Up);
 
-            PlayingCard.GetImage(PACK_PATH + "gray_back.png");
+            // TODO: Can do a lot with this, crop it and what not with CroppedBitMap
+            // Refer to: https://www.c-sharpcorner.com/UploadFile/mahesh/using-xaml-image-in-wpf/
+            // Create Image to Add to Form, set Dimensions, Source
+            var dynamicImage = new Image {Width = 70, Height = 107, Source = test_card.GetImage()};
+
+            // Add Image to Form
+            DurakRoot.Children.Add(dynamicImage);
         }
     }
 }
