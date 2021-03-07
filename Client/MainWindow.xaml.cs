65d527e06aada4537626e4b4ece7ef774cddd628
Raje Singh <raje.singh@dcmail.ca>
2021-03-07 07:01:58 -0500

    add test code for ConsoleManager and Logger classes

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,7 +26,13 @@ namespace Client
         
         public MainWindow()
         {
+            ConsoleManager.Show();  // Show the Console
+            var icoPath = Directory.GetParent(Environment.CurrentDirectory).
+                Parent.
+                FullName; // Really Scuffed Icon Path Retrieval
+            ConsoleManager.SetConsoleIcon(new Icon($"{icoPath}/icon.ico"));
             InitializeComponent();
+            Logger.Start(); // Start Logging
         }
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         { 
