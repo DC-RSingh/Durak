3e2cdd2cdaf2b12a596b9ec5d8aa66f51ea06a42
Raje Singh <raje.singh@dcmail.ca>
2021-03-09 21:21:41 -0500

    added CardBase abstract class | implement abstraction in othe classes

--- CardLib/Card.cs
+++ CardLib/Card.cs
@@ -1,124 +1,20 @@
-﻿using System;
-
-namespace CardLib
+﻿namespace CardLib
 {
-    public class Card : ICloneable
+    public class Card : CardBase
     {
-        public static bool useTrumps = false;
 
-        public static Suit trump = Suit.Club;
-
-        public static bool isAceHigh = true;
-
-        public object Clone()
+        public override object Clone()
         {
             return MemberwiseClone();
         }
 
-        public readonly Suit Suit;
-        public readonly Rank Rank;
-        public Face Face;
-
-        public Card(Suit newSuit, Rank newRank)
+        public Card(Suit newSuit, Rank newRank) : base(newSuit, newRank)
         {
-            Suit = newSuit;
-            Rank = newRank;
-            Face = Face.Up;
         }
 
-        public Card(Suit newSuit, Rank newRank, Face newFace)
+        public Card(Suit newSuit, Rank newRank, Face newFace) : base(newSuit, newRank, newFace)
         {
-            // TODO: Repeating Code, make code DRY
-            Suit = newSuit;
-            Rank = newRank;
-            Face = newFace;
         }
 
-        public override string ToString()
-        {
-            return $"Face {Face} {Rank} of {Suit}s.";
-        }
-
-        // Operator Overloads
-        public static bool operator ==(Card card1, Card card2)
-        {
-            return card1.Suit == card2.Suit && (card1.Rank == card2.Rank);
-        }
-
-        public static bool operator !=(Card card1, Card card2)
-        {
-            return !(card1 == card2);
-        }
-
-        public override bool Equals(object card)
-        {
-            return this == (Card) card;
-        }
-
-        public override int GetHashCode()
-        {
-            return 13 * (int) Suit + (int) Rank;
-        }
-
-        public static bool operator >(Card card1, Card card2)
-        {
-            if (card1.Suit == card2.Suit)
-            {
-                if (isAceHigh)
-                {
-                    if (card1.Rank == Rank.Ace)
-                    {
-                        return card2.Rank != Rank.Ace;
-                    }
-
-                    if (card2.Rank == Rank.Ace)
-                        return false;
-                    return(card1.Rank > card2.Rank);
-                }
-
-                return (card1.Rank > card2.Rank);
-            }
-
-            if (useTrumps && (card2.Suit == trump))
-                return false;
-            return true;
-
-        }
-
-        public static bool operator <(Card card1, Card card2)
-        {
-            return !(card1 >= card2);
-        }
-
-        public static bool operator >=(Card card1, Card card2)
-        {
-
-            if (card1.Suit == card2.Suit)
-            {
-                if (isAceHigh)
-                {
-                    if (card1.Rank == Rank.Ace)
-                    {
-                        return true;
-                    }
-
-                    if (card2.Rank == Rank.Ace)
-                        return false;
-                    return (card1.Rank >= card2.Rank);
-                }
-
-                return (card1.Rank >= card2.Rank);
-            }
-
-            if (useTrumps && (card2.Suit == trump))
-                return false;
-            return true;
-
-        }
-
-        public static bool operator <=(Card card1, Card card2)
-        {
-            return !(card1 > card2);
-        }
     }
 }
--- CardLib/Deck.cs
+++ CardLib/Deck.cs
@@ -1,40 +1,47 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 
 namespace CardLib
 {
     // TODO: Add Comments
-    // TODO: Configure for PlayingCards
-    public class Deck : ICloneable
+    // TODO: Configure for different deck sizes 20, 36, 52
+    public class Deck<T> : ICloneable where T : CardBase
     {
+        // TODO: 20 = 10 to Ace All Suits, 36 = 6 to Ace All Suits
+        private readonly Cards _cards = new Cards();
+
+        public int Size => _cards.Count;
+
+        public bool CanDraw => _cards.Count > 0;
+
         public object Clone()
         {
-            var newDeck = new Deck(cards.Clone() as Cards);
+            var newDeck = new Deck<T>(_cards.Clone() as Cards);
             return newDeck;
         }
 
         private Deck(Cards newCards)
         {
-            cards = newCards;
+            _cards = newCards;
         }
-
-        private Cards cards = new Cards();
 
         public Deck(bool useTrumps, Suit trump) : this()
         {
-            Card.useTrumps = useTrumps;
-            Card.trump = trump;
+            CardBase.UseTrumps = useTrumps;
+            CardBase.Trump = trump;
         }
 
         public Deck(bool isAceHigh) : this()
         {
-            Card.isAceHigh = isAceHigh;
+            CardBase.IsAceHigh = isAceHigh;
         }
 
         public Deck(bool isAceHigh, bool useTrumps, Suit trump) : this()
         {
-            Card.isAceHigh = isAceHigh;
-            Card.useTrumps = useTrumps;
-            Card.trump = trump;
+            CardBase.IsAceHigh = isAceHigh;
+            CardBase.UseTrumps = useTrumps;
+            CardBase.Trump = trump;
         }
 
         public Deck()
@@ -43,15 +50,15 @@ namespace CardLib
             {
                 for (var rankVal = 1; rankVal < 14; rankVal++)
                 {
-                    cards.Add(new Card((Suit)suitVal, (Rank)rankVal));
+                    _cards.Add((T)Activator.CreateInstance(typeof(T), (Suit)suitVal, (Rank)rankVal));
                 }
             }
         }
 
-        public Card GetCard(int cardNum)
+        public T GetCard(int cardNum)
         {
             if (cardNum >= 0 && cardNum <= 51)
-                return cards[cardNum];
+                return (T)_cards[cardNum];
    
             throw new ArgumentOutOfRangeException(nameof(cardNum), cardNum, "Value must be between 0 and 51.");
         }
@@ -77,9 +84,37 @@ namespace CardLib
                 }
 
                 assigned[destCard] = true;
-                newDeck.Add(cards[destCard]);
+                newDeck.Add(_cards[destCard]);
             }
-            newDeck.CopyTo(cards);
+            newDeck.CopyTo(_cards);
+        }
+
+        public T Draw()
+        {
+            var drawn = _cards.Last();
+
+            _cards.RemoveAt(_cards.Count - 1);
+
+            return (T)drawn;
+        }
+
+        public Cards Draw(int amount)
+        {
+            var cards = new Cards();
+
+            for (var i = 0; i < amount; i++)
+            {
+                cards.Add(Draw());
+            }
+
+            return cards;
+        }
+
+        public T DrawAt(int pos)
+        {
+            var card = GetCard(pos);
+            _cards.RemoveAt(pos);
+            return card;
         }
 
     }
