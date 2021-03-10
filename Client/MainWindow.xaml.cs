00c1dc3c79b86938f6e55653aeae85b94c2161f9
Raje Singh <raje.singh@dcmail.ca>
2021-03-09 21:23:37 -0500

    add GameplayTest class for testing game logic

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -6,15 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Client
 {
@@ -33,6 +25,8 @@ namespace Client
             ConsoleManager.SetConsoleIcon(new Icon($"{icoPath}/icon.ico"));
             InitializeComponent();
             Logger.Start(); // Start Logging
+            
+            GameplayTest.Play();
         }
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         { 
