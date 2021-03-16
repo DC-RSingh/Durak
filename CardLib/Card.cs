912918e7faba0520728393f4d22ebed06df36740
Raje Singh <raje.singh@dcmail.ca>
2021-03-15 22:34:47 -0400

    Fix bad merge

--- CardLib/Card.cs
+++ CardLib/Card.cs
@@ -1,40 +1,20 @@
-﻿using System;
-
-namespace CardLib
+﻿namespace CardLib
 {
     public class Card : CardBase
     {
-        public static bool useTrumps = false;
 
+        public override object Clone()
         {
             return MemberwiseClone();
         }
 
+        public Card(Suit newSuit, Rank newRank) : base(newSuit, newRank)
         {
-            return !(card1 >= card2);
         }
 
+        public Card(Suit newSuit, Rank newRank, Face newFace) : base(newSuit, newRank, newFace)
         {
-                        return true;
-                    }
-
-                    if (card2.Rank == Rank.Ace)
-                        return false;
-                    return (card1.Rank >= card2.Rank);
-                }
-
-                return (card1.Rank >= card2.Rank);
-            }
-
-            if (useTrumps && (card2.Suit == trump))
-                return false;
-            return true;
-
         }
 
-        public static bool operator <=(Card card1, Card card2)
-        {
-            return !(card1 > card2);
-        }
     }
 }
