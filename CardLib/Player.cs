21cacd41ffd4e5dfe07b05d3f97133128a0a4811
Raje Singh <raje.singh@dcmail.ca>
2021-03-18 02:23:21 -0400

    remove unused using directives and fields from Player

--- CardLib/Player.cs
+++ CardLib/Player.cs
@@ -1,83 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// TODO: This and DurakAI should probably be moved to a different class library
 namespace CardLib
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-
-    namespace CardLib
+    public class Player
     {
-        public class Player
+        #region Properties
+
+        public bool IsAttacking { get; set; }
+
+        /// <summary>
+        /// Represents the player's Hand
+        /// </summary>
+        public Cards Hand { get; set; }
+
+        /// <summary>
+        /// Represents the set of cards the player has in hands
+        /// </summary>
+        public int HandSize => Hand.Count;
+
+        /// <summary>
+        /// Indicates player's name
+        /// </summary>
+        public string PlayerName { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Method referring to player
+        /// </summary>
+        /// <param name="playerName">Refers to player name</param>
+        /// <param name="hand">Refers to player's hand</param>
+        public Player(string playerName, Cards hand)
         {
-            private String _playerName;
-            private String _playerType;
-
-            /// <summary>
-            /// Represents the deck
-            /// </summary>
-            private Cards _hand;
-
-            /// <summary>
-            /// Default Constructor
-            /// </summary>
-            private Player()
-            {
-                throw new System.NotImplementedException();
-            }
-
-            #region Properties
-
-            public bool IsAttacking { get; set; }
-
-            /// <summary>
-            /// Represents the player's Hand
-            /// </summary>
-            public Cards Hand { get; private set; }
-
-            /// <summary>
-            /// Represents the set of cards the player has in hands
-            /// </summary>
-            public int HandSize => Hand.Count;
-
-            /// <summary>
-            /// Indicates player's name
-            /// </summary>
-            public string PlayerName { get; private set; }
-
-            /// <summary>
-            /// Indicates player's type
-            /// </summary>
-            public string PlayerType
-            {
-                get => default;
-                set
-                {
-                }
-            }
-
-            #endregion
-
-            #region Constructors
-
-            /// <summary>
-            /// Method referring to player
-            /// </summary>
-            /// <param name="playerName">Refers to player name</param>
-            /// <param name="hand">Refers to player's hand</param>
-            public Player(string playerName, Cards hand)
-            {
-                PlayerName = playerName;
-                Hand = hand;
-            }
-
-            #endregion
-
+            PlayerName = playerName;
+            Hand = hand;
         }
+
+        #endregion
+
     }
 }
