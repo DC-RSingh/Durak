032d11b1edf7428474fb4da10fdb6b2b84a531bb
Raje Singh <raje.singh@dcmail.ca>
2021-03-18 02:47:10 -0400

    add DurakAI class

---
+++ CardLib/DurakAI.cs
@@ -0,0 +1,31 @@
+﻿// TODO: This should probably be moved to a different class library
+namespace CardLib
+{
+    public class DurakAI : Player
+    {
+
+        public DurakAI(string name, Cards hand) : base(name, hand)
+        {
+        }
+
+        public void TakeTurn(Cards river)
+        {
+            var playable = (Cards) Hand.FindAll(card => IsPlayable(river, card));
+            if (IsAttacking)
+                Attacking(river, playable);
+            else
+                Defending(river, playable);
+        }
+
+        private void Attacking(Cards river, Cards playableCards)
+        {
+
+        }
+
+        private void Defending(Cards river, Cards playableCards)
+        {
+
+        }
+
+    }
+}
