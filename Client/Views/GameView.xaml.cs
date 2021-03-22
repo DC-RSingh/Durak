7666724d99c3389259cae304f0539c22d16bd62d
Raje Singh <raje.singh@dcmail.ca>
2021-03-22 19:05:43 -0400

    cleanup code after merge

--- Client/Views/GameView.xaml.cs
+++ Client/Views/GameView.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CardLib;
-using CardLib.CardLib;
 using CardUI;
-using Client.ViewModels;
+using DurakLib;
 
 namespace Client.Views
 {
@@ -263,7 +251,7 @@ namespace Client.Views
             img.Width = regularWidth;
         }
 
-        private void DisplayDeck(Deck<CardBase> s)
+        private void DisplayDeck<T>(Deck<T> s) where T : Card
         {
             for (var i = 0; i < s.Size; i++)
             {
