8c6de6f9877f802c4ae37fe68c9b2f74e65b2d4f
Raje Singh <raje.singh@dcmail.ca>
2021-03-21 23:41:45 -0400

    add DeckSize enum

---
+++ CardLib/DeckSize.cs
@@ -0,0 +1,22 @@
+﻿namespace CardLib
+{
+    /// <summary>
+    /// Represents different sizes a deck can have, with the value of each member indicating the ranks in the deck.
+    /// </summary>
+    public enum DeckSize
+    {
+        Four = 1,
+        Eight,
+        Twelve,
+        Sixteen,
+        Twenty,
+        TwentyFour,
+        TwentyEight,
+        ThirtyTwo,
+        ThirtySix,
+        Forty,
+        FortyFour,
+        FortyEight,
+        FiftyTwo
+    }
+}
