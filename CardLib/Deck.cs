1c5fa70042af66708252b00cb70d0328ae904e5a
Raje Singh <raje.singh@dcmail.ca>
2021-03-22 02:00:11 -0400

    add documentation to Deck

--- CardLib/Deck.cs
+++ CardLib/Deck.cs
@@ -3,7 +3,6 @@ using System.Linq;
 
 namespace CardLib
 {
-    // TODO: Add Comments
     /// <summary>
     /// Represents a Deck of <see cref="Card"/> that emulates Playing Card deck functionality.
     /// </summary>
@@ -21,17 +20,35 @@ namespace CardLib
         /// </summary>
         public event EventHandler DeckShuffled;
 
-
+        /// <summary>
+        /// Event that is invoked when a card is drawn from the deck.
+        /// <para>
+        /// Never invoked if the last card in the deck is drawn, <see cref="LastCardDrawn"/> is invoked instead.
+        /// </para>
+        /// </summary>
         public event EventHandler CardDrawn;
 
+        // Readonly collection of Cards
         private readonly Cards _cards = new Cards();
 
+        /// <summary>
+        /// The current size of the deck.
+        /// </summary>
         public int Size => _cards.Count;
 
+        /// <summary>
+        /// The type/<see cref="DeckSize"/> of the deck.
+        /// </summary>
         public DeckSize DeckType { get; private set; }
 
+        /// <summary>
+        /// The size the deck was before any cards were drawn.
+        /// </summary>
         public int OriginalDeckSize => (int)DeckType * 4;
 
+        /// <summary>
+        /// Whether or not it would be a valid operation to draw a card.
+        /// </summary>
         public bool CanDraw => _cards.Count > 0;
 
         public object Clone()
@@ -40,22 +57,42 @@ namespace CardLib
             return newDeck;
         }
 
+        // Private constructor used to initialize a deck from a Cards collection.
         private Deck(Cards newCards)
         {
             _cards = newCards;
         }
 
+        /// <summary>
+        /// Instantiates an instance of a deck of the specified size and sets the <see cref="CardBase.UseTrumps"/> property.
+        /// and <see cref="CardBase.Trump"/>.
+        /// </summary>
+        /// <param name="useTrumps">Whether to use trumps or not.</param>
+        /// <param name="trump">The trump suit to use, set to <see cref="Suit.Blank"/> by default.</param>
+        /// <param name="size">The size of the deck, set to <see cref="DeckSize.FiftyTwo"/> by default.</param>
         public Deck(bool useTrumps, Suit trump = Suit.Blank, DeckSize size = DeckSize.FiftyTwo) : this(size)
         {
             CardBase.UseTrumps = useTrumps;
             CardBase.Trump = trump;
         }
 
+        /// <summary>
+        /// Instantiates an instance of a deck of the specified size and sets the <see cref="CardBase.IsAceHigh"/> property.
+        /// </summary>
+        /// <param name="isAceHigh">Whether the value of an ace is high or not.</param>
+        /// <param name="size">The size of the deck, set to <see cref="DeckSize.FiftyTwo"/> by default.</param>
         public Deck(bool isAceHigh, DeckSize size = DeckSize.FiftyTwo) : this(size)
         {
             CardBase.IsAceHigh = isAceHigh;
         }
 
+        /// <summary>
+        /// Instantiates an instance of a deck with all the static properties of <see cref="CardBase"/> set.
+        /// </summary>
+        /// <param name="isAceHigh">Whether the value of an ace is high or not.</param>
+        /// <param name="useTrumps">Whether to use trumps or not.</param>
+        /// <param name="trump">The trump suit to use, set to <see cref="Suit.Blank"/> by default.</param>
+        /// <param name="size">The size of the deck, set to <see cref="DeckSize.FiftyTwo"/> by default.</param>
         public Deck(bool isAceHigh, bool useTrumps, Suit trump = Suit.Blank, DeckSize size = DeckSize.FiftyTwo) : this(size)
         {
             CardBase.IsAceHigh = isAceHigh;
@@ -64,9 +101,9 @@ namespace CardLib
         }
 
         /// <summary>
-        /// 
+        /// Instantiates an instance of a deck of <see cref="DeckSize.FiftyTwo"/>, or the specified size if any.
         /// </summary>
-        /// <param name="size"></param>
+        /// <param name="size">The size of the deck.</param>
         public Deck(DeckSize size = DeckSize.FiftyTwo)
         {
             DeckType = size;
@@ -74,8 +111,10 @@ namespace CardLib
             for (var suitVal = 0; suitVal < 4; suitVal++)
             {
                 // Add the ACE for that suit
+                // Activator.CreateInstance attempts to invoke a constructor of the specified type with the specified params.
                 _cards.Add((T)Activator.CreateInstance(typeof(T), (Suit)suitVal, (Rank)1));
 
+                // Adds the remaining ranks for each suit, starting from the highest suit.
                 for (var rankVal = 13; rankVal > 13 - (int)size + 1; rankVal--)
                 {
                     _cards.Add((T)Activator.CreateInstance(typeof(T), (Suit)suitVal, (Rank)rankVal));
@@ -83,6 +122,11 @@ namespace CardLib
             }
         }
 
+        /// <summary>
+        /// Gets the card at the specified position. Indexes start at 0.
+        /// </summary>
+        /// <param name="cardNum">The position of the card.</param>
+        /// <returns>The card at the specified position.</returns>
         public T GetCard(int cardNum)
         {
             if (cardNum >= 0 && cardNum <= Size)
@@ -91,6 +135,10 @@ namespace CardLib
             throw new ArgumentOutOfRangeException(nameof(cardNum), cardNum, $"Value must be between 0 and {Size}.");
         }
 
+        /// <summary>
+        /// Shuffles the cards in the deck, placing them in a random order.
+        /// <para>Invokes the <see cref="DeckShuffled"/> event.</para>
+        /// </summary>
         public void Shuffle()
         {
             var newDeck = new Cards();
@@ -118,9 +166,16 @@ namespace CardLib
             DeckShuffled?.Invoke(this, EventArgs.Empty);
         }
 
-        // TODO: Make a note that it throws an exception if the cards are out of range
+        /// <summary>
+        /// Draws the card at the top of the deck (the last element in the sequence). 
+        /// </summary>
+        /// <remarks>Removes the card in the process.</remarks>
+        /// <returns>The card at the top of the deck. Returns null if there are no cards in the deck.</returns>
+        /// /// <exception cref="InvalidOperationException"></exception>
         public T Draw()
         {
+            if (!CanDraw) throw new InvalidOperationException("The deck is empty, no more cards can be drawn!");
+
             var drawn = _cards.Last();
 
             _cards.RemoveAt(Size - 1);
@@ -129,6 +184,13 @@ namespace CardLib
             return (T)drawn;
         }
 
+        /// <summary>
+        /// Draws the specified amount of cards from the top of the deck
+        /// </summary>
+        /// <remarks>Removes the cards in the process.</remarks>
+        /// <param name="amount">The amount of cards to draw from the top of the deck.s</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <returns>A <see cref="Cards"/> collection object with the cards drawn.</returns>
         public Cards Draw(int amount)
         {
             var cards = new Cards();
@@ -144,6 +206,12 @@ namespace CardLib
             return cards;
         }
 
+        /// <summary>
+        /// Draws the card at the specified position in the deck.
+        /// </summary>
+        /// <remarks>Removes the card from the deck.</remarks>
+        /// <param name="pos">The position of the card to draw.</param>
+        /// <returns>The card at the specified position.</returns>
         public T DrawAt(int pos)
         {
             var card = GetCard(pos);
@@ -152,9 +220,10 @@ namespace CardLib
             return card;
         }
 
+        // Invokes either LastCardDrawn or CardDrawn depending on the state of the Deck object.
         private void InvokeDrawEvents(EventArgs e)
         {
-            if ( _cards.Count == 1)
+            if ( _cards.Count == 0)
                 LastCardDrawn?.Invoke(this, e);
             else
                 CardDrawn?.Invoke(this, e);
