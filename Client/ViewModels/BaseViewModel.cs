397fccbd3e26b74c3dc288cb7001a31f9da0458f
Raje Singh <raje.singh@dcmail.ca>
2021-04-13 02:04:05 -0400

    add BaseViewModel class

---
+++ Client/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using PropertyChanged;
+
+namespace Client.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
