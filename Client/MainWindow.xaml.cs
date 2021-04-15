2b8f889f8d6745f21c3f8ebbbb146e63031572cb
Raje Singh <raje.singh@dcmail.ca>
2021-04-15 09:44:32 -0400

    comment out game abort prompt

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -26,14 +26,14 @@ namespace Client
 
         private void PlayMenu_Click(object sender, RoutedEventArgs e)
         {
-            CheckIfGameExists();
+            //CheckIfGameExists();
             GameView gameView = new GameView(Statistics.DeckSize, Statistics.PlayerName);
             DataContext = gameView;
         }
 
         private void AboutMenu_Click(object sender, RoutedEventArgs e)
         {
-            CheckIfGameExists();
+            //CheckIfGameExists();
             AboutView aboutView = new AboutView();
             DataContext = aboutView;
         }
@@ -49,7 +49,7 @@ namespace Client
 
         private void GameOptionsMenu_Click(object sender, RoutedEventArgs e)
         {
-            CheckIfGameExists();
+            //CheckIfGameExists();
             GameSettingsView gameSettingsView = new GameSettingsView();
             DataContext = gameSettingsView;
         }
@@ -59,7 +59,7 @@ namespace Client
         }
         private void MainMenu_Click(object sender, RoutedEventArgs e)
         {
-            CheckIfGameExists();
+            //CheckIfGameExists();
             MainPageView dataContext = new MainPageView();
             DataContext = dataContext;
         }
